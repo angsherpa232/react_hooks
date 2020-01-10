c07aa7631a1d6979af8ed684da6aac9a670f447c
angsherpa232 <angsherpa232@gmail.com>
2020-01-10 22:27:50 +0100

    setTimer for the data fetching. This prevents the request to the server for every single keystroke change. Also in the useEffect in Search Component, funcation is returned in order to clean the setTimer that gets generated for each keystroke pressed.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -7,28 +7,38 @@ const Search = React.memo(props => {
   const [enteredFilter, setEnteredFilter] = useState("");
   const { onLoadIngredients } = props;
 
+  const inputRef = useRef();
+
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(
-      "https://marker-1516988810351.firebaseio.com/ingredients.json" + query
-    )
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        onLoadIngredients(loadedIngredients);
-      })
-      .catch(error => console.log(error));
-  }, [enteredFilter, onLoadIngredients]);
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://marker-1516988810351.firebaseio.com/ingredients.json" + query
+        )
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          })
+          .catch(error => console.log(error));
+      }
+    }, 500);
+    // Clear the old timer that is created for every key stroke pressed
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -36,6 +46,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={e => setEnteredFilter(e.target.value)}
