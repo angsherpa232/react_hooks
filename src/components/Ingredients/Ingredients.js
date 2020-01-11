6999766704f2029da872a06140612983eb097f12
angsherpa232 <angsherpa232@gmail.com>
2020-01-11 13:00:27 +0100

    useReducer used for setIngredients method of useState.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useReducer } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error("Should not get here");
+  }
+};
+
 function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
   const filteredIngredientHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
   useEffect(() => console.log("RENDERING INGREDIENTS", userIngredients), [
@@ -32,13 +47,20 @@ function Ingredients() {
         return response.json();
       })
       .then(responseData => {
-        setUserIngredients(prevIngredients => [
-          ...prevIngredients,
-          {
+        // setUserIngredients(prevIngredients => [
+        //   ...prevIngredients,
+        //   {
+        //     id: responseData.name,
+        //     ...ingredient
+        //   }
+        // ]);
+        dispatch({
+          type: "ADD",
+          ingredient: {
             id: responseData.name,
             ...ingredient
           }
-        ]);
+        });
       })
       .catch(error => {
         setIsLoading(false);
@@ -49,16 +71,17 @@ function Ingredients() {
   const removeIngredientHandler = ingredientId => {
     setIsLoading(true);
     fetch(
-      `https://marker-1516988810351.firebaseio.com/ingredients/${ingredientId}.jon`,
+      `https://marker-1516988810351.firebaseio.com/ingredients/${ingredientId}.json`,
       {
         method: "DELETE"
       }
     )
       .then(response => {
         setIsLoading(false);
-        setUserIngredients(prevIngredients =>
-          prevIngredients.filter(item => item.id !== ingredientId)
-        );
+        // setUserIngredients(prevIngredients =>
+        //   prevIngredients.filter(item => item.id !== ingredientId)
+        // );
+        dispatch({ type: "DELETE", id: ingredientId });
       })
       .catch(error => {
         setIsLoading(false);
